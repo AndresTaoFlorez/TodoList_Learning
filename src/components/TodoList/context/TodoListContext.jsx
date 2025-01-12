b76395a40eef2618c648037cd5a752870a406cbd
Andres Tao <andrestao577@gmail.com>
2025-01-11 22:52:11 -0500

    TodoList Stable v0004

--- src/components/TodoList/context/TodoListContext.jsx
+++ src/components/TodoList/context/TodoListContext.jsx
@@ -1,52 +1,40 @@
 import { createContext, useEffect, useState } from "react";
 import { useItems } from "../../../customHooks/useItems";
-import { pharagraph } from '../text/pharagraphArray'
+import { pharagraph } from '../text/pharagraphArray';
 import { getLocalConfig, setLocalConfig } from "../../../services/localStorage";
 
-const TodoListContext = createContext()
+const TodoListContext = createContext();
 
 const TodoListProvider = ({ children }) => {
-
-  const [config, setConfig] = useState()
+  const [config, setConfig] = useState();
   const [view, setView] = useState(getLocalConfig().view || false);
 
+  const detectLanguage = () => {
+    const language = navigator.language || navigator.userLanguage;
+    const spanishSpeakingCountries = [
+      'AR', 'BO', 'CL', 'CO', 'CR', 'DO', 'EC', 'SV', 'GT', 'HN', 'MX', 'NI', 'PA', 'PY', 'PE', 'PR', 'UY', 'VE'
+    ];
+    return language.startsWith('es') || spanishSpeakingCountries.includes(language.split('-')[1]?.toUpperCase()) ? 'ES' : 'EN';
+  };
+
   useEffect(() => {
-    setConfig({
-      pharagraph: pharagraph.ES.map(obj => Object.values(obj)[0]) || ''
-    });
+    const language = detectLanguage();
+    setConfig({ pharagraph: pharagraph[language].map(obj => Object.values(obj)[0]) || '' });
   }, []);
-  
+
   useEffect(() => {
     setLocalConfig({ items: { view } });
   }, [view]);
-  
 
-  const {
-    handleComplete = () => { },
-    handleDelete = () => { },
-    handleSearch = () => { },
-    handleAdd = () => { },
-    itemsFinded: todos = [],
-    loading = false,
-    errors = null
-  } = useItems()
+  const { handleComplete, handleDelete, handleSearch, handleAdd, itemsFinded: todos = [], loading = false, errors = null } = useItems();
 
   return (
     <TodoListContext.Provider value={{
-      handleComplete,
-      handleDelete,
-      handleSearch,
-      handleAdd,
-      itemsFinded: todos,
-      loading,
-      errors,
-      config,
-      setView,
-      view
+      handleComplete, handleDelete, handleSearch, handleAdd, itemsFinded: todos, loading, errors, config, setView, view
     }}>
       {children}
     </TodoListContext.Provider>
-  )
-}
+  );
+};
 
-export { TodoListContext, TodoListProvider }
+export { TodoListContext, TodoListProvider };
